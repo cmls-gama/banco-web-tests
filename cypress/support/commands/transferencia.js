17fdc3cc257a116c304c99426dc4d6b31efcfae0
Caio Marques <cmls.marques@gmail.com>
2026-07-03 14:13:22 -0300

    Implmentando comandos customizações para login, transferencias e commons

---
+++ cypress/support/commands/transferencia.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('realizarTransferencia',(contaOrigem, contaDestino, valor)=>{
+    cy.selecionarOpcaoNaComboBox('conta-origem',contaOrigem)
+    cy.selecionarOpcaoNaComboBox('conta-destino',contaDestino)
+    cy.get('#valor').click().type(valor)
+    cy.contains('button', 'Transferir').click()
+
+})
